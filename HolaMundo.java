eef9766b7e42e9aa872814963028df4ba254a527
Jurdi5 <jorge.urdiales@iteso.mx>
2025-09-17 10:40:58 -0600

    Update HolaMundo.java

--- HolaMundo.java
+++ HolaMundo.java
@@ -1,8 +1,7 @@
-package tutorials;
 
 public class HolaMundo {
     public static void main(String[] args) {
-        System.out.println("Hola Mundo!");
+        System.out.println("Hola Mundo");
     }
 
 }
